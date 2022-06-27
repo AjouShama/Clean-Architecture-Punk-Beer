ae0a127fac7ebfcd2c0130a064d944a7479d987e
AjouShama <ajoushama@gmail.com>
2022-06-27 21:50:31 +0900

    SpecificPunkBeerUseCase 추가

--- domain/src/main/java/com/ray/beer/domain/usecase/beer/GetSpecificPunkBeerUseCase.kt
+++ domain/src/main/java/com/ray/beer/domain/usecase/beer/GetSpecificPunkBeerUseCase.kt
@@ -1,6 +1,6 @@
 package com.ray.beer.domain.usecase.beer
 
-import com.ray.beer.core.di.NonCachedPunkBeer
+import com.ray.beer.core.di.CachedPunkBeer
 import com.ray.beer.domain.model.beer.PunkBeerPure
 import com.ray.beer.domain.model.common.ResponseCover
 import com.ray.beer.domain.repository.beer.PunkBeerRepository
@@ -11,11 +11,11 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOn
 
 @Reusable
-class GetRandomPunkBeerUseCase @Inject constructor(
-    @NonCachedPunkBeer private val punkBeerRepository: PunkBeerRepository
+class GetSpecificPunkBeerUseCase @Inject constructor(
+    @CachedPunkBeer private val punkBeerRepository: PunkBeerRepository
 ) {
-    operator fun invoke(): Flow<ResponseCover<PunkBeerPure>> {
-        return punkBeerRepository.getRandomPunkBeer()
+    operator fun invoke(name: String): Flow<ResponseCover<PunkBeerPure>> {
+        return punkBeerRepository.getPunkBeerByName(name)
             .flowOn(Dispatchers.IO)
     }
 }
--- presentation/src/main/java/com/ray/beer/presentation/ui/main/MainViewModel.kt
+++ presentation/src/main/java/com/ray/beer/presentation/ui/main/MainViewModel.kt
@@ -6,30 +6,37 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ray.beer.domain.model.beer.PunkBeerPure
 import com.ray.beer.domain.usecase.beer.GetRandomPunkBeerUseCase
+import com.ray.beer.domain.usecase.beer.GetSpecificPunkBeerUseCase
+import com.ray.beer.presentation.ui.common.util.valueOrDefault
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val getRandomPunkBeerUseCase: GetRandomPunkBeerUseCase
+    private val getRandomPunkBeerUseCase: GetRandomPunkBeerUseCase,
+    private val getSpecificPunkBeerUseCase: GetSpecificPunkBeerUseCase
 ) : ViewModel() {
 
     private val _punkBeer = MutableLiveData<PunkBeerPure>()
     val punkBeer: LiveData<PunkBeerPure>
         get() = _punkBeer
 
-    init {
-        getRandomPunkBeer()
-    }
+    val punkBeerName: MutableLiveData<String> = MutableLiveData()
 
-    fun onRetryClick() {
-        getRandomPunkBeer()
-    }
-
-    private fun getRandomPunkBeer() {
+    fun onRandomPunkBeerClick() {
         viewModelScope.launch {
-            getRandomPunkBeerUseCase.invoke().collect {
+            getRandomPunkBeerUseCase().collect {
+                if (it.isSuccess) {
+                    _punkBeer.value = it.data ?: PunkBeerPure.default
+                }
+            }
+        }
+    }
+
+    fun onSpecificPunkBeerClick() {
+        viewModelScope.launch {
+            getSpecificPunkBeerUseCase(punkBeerName.valueOrDefault).collect {
                 if (it.isSuccess) {
                     _punkBeer.value = it.data ?: PunkBeerPure.default
                 }
